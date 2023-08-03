f17464b2341bb05651b3ec0ca935ce8adfc79799
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-03 16:40:37 +0530

    ypdate in ui and added icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-
+import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
 let getLocalStorageData = () => {
   let arr = localStorage.getItem("todoListItems")
   if (arr) {
@@ -37,19 +37,19 @@ function App() {
     }
 
     // due to update
-    else if(inputData && toggleBtn==true){
+    else if (inputData && toggleBtn == true) {
       setitems(
-        items.map((x)=>{
-          if(x.id==id_of_edit_item){
-            return {...x , data:inputData}
+        items.map((x) => {
+          if (x.id == id_of_edit_item) {
+            return { ...x, data: inputData }
           }
-          else{
+          else {
             return x
           }
         })
       )
       // now matlba update complete so make normal
-      setid_of_edit_item(null) ; 
+      setid_of_edit_item(null);
       setinputData("")
       settoggleBtn(false)
     }
@@ -69,14 +69,14 @@ function App() {
 
   let updatefnc = (parameter) => {
 
-    let item_to_edit = items.find((x)=>{
-      return x.id===parameter; 
+    let item_to_edit = items.find((x) => {
+      return x.id === parameter;
     })
-    setid_of_edit_item(parameter) ; 
+    setid_of_edit_item(parameter);
     setinputData(item_to_edit.data)
     // matlab jab update pe click ho add wala btn update me convert ho jaye
     settoggleBtn(true)
-   
+
   }
 
   let deletefnc = (parameter) => {
@@ -102,29 +102,39 @@ function App() {
 
   return (
     <>
-      <h1>Todo List</h1>
-      <h3>Add your itemss</h3>
+      <div className='heading' >
+
+        <h1>
+          Hello <img className='h1' src="./images/wavyHand.png" alt=" " />
+          Welcome to your All in one Task Manager
+        </h1>
+
+        <h3>Add your Work Here 👍</h3>
 
 
-      <input type="text" value={inputData} onChange={onchange} placeholder='Enter your data' />
+        <div>
+          <input type="text" value={inputData} onChange={onchange} placeholder={`${"Add Your Item"}`} />
 
-{
- (toggleBtn==false)? ( <button onClick={addBtn}>Add</button>) : ( <button onClick={addBtn}>update</button>)
-}
-     
+          {
+            (toggleBtn == false) ? (<button onClick={addBtn}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></button>) : (<button onClick={addBtn}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg></button>)
+          }
 
-      {
-        items.map((element, index) => {
-          return (
-            <div className="list" key={index}>
-              <div className="items1">{element.data}</div>
-              <button onClick={() => updatefnc(element.id)}>edit</button>
-              <button onClick={() => deletefnc(element.id)}>delete</button>
-            </div>
-          )
-        })
-      }
+        </div>
+      </div>
 
+      <div className="list">
+        {
+          items.map((element, index) => {
+            return (
+              <div className="sub_list" key={index}>
+                <div className="data">{element.data}</div>
+                <button onClick={() => updatefnc(element.id)}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg></button>
+                <button onClick={() => deletefnc(element.id)}><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button>
+              </div>
+            )
+          })
+        }
+      </div>
 
       <button onClick={() => { deletefnc() }}>CheckList</button>
 
